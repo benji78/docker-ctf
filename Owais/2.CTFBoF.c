4a0efaf8ae545e011ecf5b4c4155a188ec8ff9ea
HanjiOS <56694986+WULFRICBLACK@users.noreply.github.com>
2025-01-19 18:41:09 +0100

    Add files via upload

---
+++ Owais/2.CTFBoF.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void hidden_shell() {
+    
+    char flag_base64[] = "ZmxhZ3tiMGZfc2hlbGxfYWNjZXNzfQ==";
+    printf("Congratulations! Here is your encoded flag: %s\n", flag_base64);
+
+    system("/bin/sh");
+}
+
+int main() {
+    char buffer[64];
+
+    printf("Enter some text: ");
+    gets(buffer); // Vulnerable: can overflow 'buffer'
+
+    printf("You entered: %s\n", buffer);
+    return 0;
+}
