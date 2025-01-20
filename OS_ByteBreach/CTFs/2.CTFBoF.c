5d133947bebc604dbf3ba8a1fa46aec94f866982
HanjiOS <56694986+WULFRICBLACK@users.noreply.github.com>
2025-01-20 18:23:54 +0100

    Update 2.CTFBoF.c

--- OS_ByteBreach/CTFs/2.CTFBoF.c
+++ OS_ByteBreach/CTFs/2.CTFBoF.c
@@ -3,7 +3,6 @@
 #include <string.h>
 
 void hidden_shell() {
-    
     char flag_base64[] = "ZmxhZ3tiMGZfc2hlbGxfYWNjZXNzfQ==";
     printf("Congratulations! Here is your encoded flag: %s\n", flag_base64);
 
@@ -14,7 +13,14 @@ int main() {
     char buffer[64];
 
     printf("Enter some text: ");
-    gets(buffer); // Vulnerable: can overflow 'buffer'
+    // Use fgets instead of gets, specifying the buffer size
+    if (fgets(buffer, sizeof(buffer), stdin) != NULL) {
+        // Remove the newline character added by fgets
+        buffer[strcspn(buffer, "\n")] = '\0';
+    } else {
+        printf("Error reading input.\n");
+        return 1;
+    }
 
     printf("You entered: %s\n", buffer);
     return 0;
